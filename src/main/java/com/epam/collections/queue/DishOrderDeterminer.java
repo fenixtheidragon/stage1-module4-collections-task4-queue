b722e3013937371a12eea40dd64e90fb0fbddec1
fenix <fenixtheidragon@gmail.com>
2023-10-12 17:45:23 +0300

    Queues and deques trying to solve DishOrderDeterminer

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -5,26 +5,43 @@ import java.util.*;
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
         System.out.println(numberOfDishes + "|||" + everyDishNumberToEat);
-        LinkedList<String> dishes = new LinkedList<>();
+
+        LinkedList<Integer> dishes = new LinkedList<>();
         LinkedList<Integer> eatenDishes = new LinkedList<>();
-        int nextDishToEat = everyDishNumberToEat;
-        for (int a = 0; a < numberOfDishes; a++) {
-            dishes.add(String.valueOf(a + 1));
+
+        for (int a = 1; a <= numberOfDishes; a++) {
+            dishes.add(a);
         }
-        dishes.forEach(a-> System.out.print(a + ", "));
-        System.out.println();
+        int absoluteBreakCount = 0;
+        int nextDishIndexLoop = 0;
+        int nextDishIndex = 0;
+
         while (!dishes.isEmpty()) {
-            if (dishes.contains(String.valueOf(nextDishToEat))) {
-                eatenDishes.add(nextDishToEat);
-                dishes.remove(String.valueOf(nextDishToEat));
-                nextDishToEat += everyDishNumberToEat;
-                System.out.print(nextDishToEat + ", ");
-            } else {
-                nextDishToEat++;
+            absoluteBreakCount++;
+            if (absoluteBreakCount >= 500) {
+                System.out.println("Infinite loop");
+                break;
             }
-            if (nextDishToEat > numberOfDishes) {
-                nextDishToEat -= numberOfDishes;
+            System.out.println(nextDishIndex + "|" + nextDishIndexLoop);
+            if (nextDishIndexLoop == everyDishNumberToEat - 1) {
+                if (dishes.isEmpty()) {
+                    break;
+                }
+                System.out.println(dishes.get(nextDishIndex));
+                eatenDishes.add(dishes.remove(nextDishIndex));
+                System.out.println(eatenDishes.getLast());
+                System.out.println("nextDishIndexLoop rotation|");
+                nextDishIndexLoop = 0;
+                continue;
             }
+            if (nextDishIndex >= dishes.size() - 1) {
+                System.out.println("nextDishIndex rotation|" + dishes.size());
+                nextDishIndex = 0;
+                //nextDishIndexLoop++;
+                continue;
+            }
+            nextDishIndexLoop++;
+            nextDishIndex++;
         }
         return eatenDishes;
     }
