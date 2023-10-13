766cb150d55043814f6cd2342d45396096b78913
fenix <fenixtheidragon@gmail.com>
2023-10-13 14:52:27 +0300

    Queues and deques trying to solve DishOrderDeterminer

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -5,33 +5,24 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+    private ArrayDeque<Integer> arrayDeque;
+
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-        for (Integer num: firstQueue) {
-            System.out.print(num + ",");
-        }
-        System.out.println("----");
-        for (Integer num: secondQueue) {
-            System.out.print(num + ",");
-        }
-        System.out.println("----");
-        arrayDeque.add(firstQueue.poll());
-        arrayDeque.add(firstQueue.poll());
-        arrayDeque.add(secondQueue.poll());
-        arrayDeque.add(secondQueue.poll());
+        this.arrayDeque = new ArrayDeque<>();
+
+        this.addQueuePollToArrayDequeTwice(firstQueue);
+        this.addQueuePollToArrayDequeTwice(secondQueue);
         while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
             firstQueue.add(arrayDeque.pollLast());
-            arrayDeque.add(firstQueue.poll());
-            arrayDeque.add(firstQueue.poll());
-
+            this.addQueuePollToArrayDequeTwice(firstQueue);
             secondQueue.add(arrayDeque.pollLast());
-            arrayDeque.add(secondQueue.poll());
-            arrayDeque.add(secondQueue.poll());
-
-        }
-        for (Integer num: arrayDeque) {
-            System.out.print(num + ",");
+            this.addQueuePollToArrayDequeTwice(secondQueue);
         }
         return arrayDeque;
+    }
+
+    private void addQueuePollToArrayDequeTwice(Queue<Integer> queue) {
+        this.arrayDeque.add(queue.poll());
+        this.arrayDeque.add(queue.poll());
     }
 }
