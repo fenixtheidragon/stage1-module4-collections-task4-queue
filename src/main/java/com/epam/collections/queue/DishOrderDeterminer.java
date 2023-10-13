733a033eb42f4f7a6afa0f43bfc4205b6d74443f
fenix <fenixtheidragon@gmail.com>
2023-10-13 14:35:15 +0300

    Queues and deques trying to solve DishOrderDeterminer

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -4,27 +4,41 @@ import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
-        LinkedList<Integer> dishes = new LinkedList<>();
+        ArrayList<Integer> dishes = new ArrayList<>();
         for (int i = 1; i <= numberOfDishes; i++) {
             dishes.add(i);
         }
         LinkedList<Integer> eatenDishes = new LinkedList<>();
         //int totalCountOfIterationsBeforeAutoBreak = 0;
         int countForDishLoop = 1;
-        while (!dishes.isEmpty()) {
-            /*totalCountOfIterationsBeforeAutoBreak++;
+        while (eatenDishes.size() != dishes.size()) {
+        /*totalCountOfIterationsBeforeAutoBreak++;
             if (totalCountOfIterationsBeforeAutoBreak > 100) {
                 break;
             }*/
             for (int i = 0; i < dishes.size(); i++) {
                 //System.out.println(countForDishLoop + "|" + i + "|" + dishes.get(i) + "||1");
+                if (dishes.get(i) == 0) {
+                    continue;
+                }
                 if (countForDishLoop == 4) {
-                    eatenDishes.add(dishes.remove(i));
+                    countForDishLoop = 0;
+                    //for (int a = 0; a < 100; a++) {
+                        if (dishes.get(i) != 0) {
+                            eatenDishes.add(dishes.get(i));
+                            dishes.set(i, 0);
+                            break;
+                        } else {
+                            i++;
+                            if (i >= dishes.size() - 1) {
+                                i = 0;
+                            }
+                        }
+                    //}
                     //System.out.println(eatenDishes.getLast());
-                    countForDishLoop = 1;
-                    if (i == dishes.size()) {
+                    /*if (i == dishes.size()) {
                         countForDishLoop = 0;
-                    }
+                    }*/
                 }
                 countForDishLoop++;
                 //System.out.println(countForDishLoop + "|" + i + "|" + dishes.get(i) + "||2");
